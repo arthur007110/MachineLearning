efd19aae135150ac5179893cd4b641591654074b
arthur007110 <arthurcosta007110@gmail.com>
2023-06-05 23:28:17 -0300

    resolucao

--- clasificador.py
+++ clasificador.py
@@ -1,4 +1,30 @@
-import math
+
+def rateCar(car):
+  buying, maint, doors, persons, lug_boot, safety, rating = car
+
+  if(safety == 'low'):
+    return 'unacc'
+  if(persons == '2'):
+    return 'unacc'
+  
+  if(maint == 'vhigh'):
+    return 'unacc'
+  
+  if(buying in ['high', 'vhigh']):
+    return 'acc'
+
+  if(lug_boot != 'small' and safety == 'high'):
+    return 'vgood'
+
+  if(maint in ['low', 'med'] ):
+    return 'good'
+    
+  return 'acc'
+
+def printCar(car, end='[]\n'):
+  print(' \t'.join(car), end=end)
+
+
 filename = 'car.data'
 
 infile = open(filename, 'r')
@@ -8,35 +34,48 @@ infile.close()
 lines = data.split('\n')
 cars = [line.split(',') for line in lines]
 
-car_atributes = dict()
 
-car_atributes['buying'] = ['vhigh', 'high', 'med', 'low']
-car_atributes['maint'] = ['vhigh', 'high', 'med', 'low']
-car_atributes['doors'] = ['2', '3', '4', '5more']
-car_atributes['persons'] = ['2', '4', 'more']
-car_atributes['lug_boot'] = ['small', 'med', 'big']
-car_atributes['safety'] = ['low', 'med', 'high']
 
 max_score = 20
 
 car_atributes_list = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety']
 
 score_classes = ['unacc', 'acc', 'good', 'vgood', 'vgood']
-score_weights = [1.5, 1.5, 0.5, 0.5, 0.5, 1]
 
-points = 0
-total_cars = 1728
+classified_cars = dict()
+total_classified_cars = {
+  'unacc': 1210,
+  'acc': 384,
+  'good': 69,
+  'vgood': 65
+}
+
+correct_classified_cars = {
+  'unacc': 0,
+  'acc': 0,
+  'good': 0,
+  'vgood': 0
+}
+
+correct = 0
+total_cars = len(cars)
 for car in cars:
-  score = 0
-  for index, atribute in enumerate(car[:-1]):
-    atribute_name = car_atributes_list[index]
-    score += car_atributes[atribute_name].index(atribute) * score_weights[index]
+  car_class = rateCar(car)
 
-  car_class_index = math.floor(score/5.1)
-  car_class = score_classes[car_class_index]
-  if(car_class == car[-1]):
-    points +=1
+  if(car_class not in classified_cars):
+    classified_cars[car_class] = 1
   else:
-    print(car, car_class)
+    classified_cars[car_class] += 1
+  
+  if(car_class == car[-1]):
+    correct_classified_cars[car_class] += 1
+    correct += 1
 
-print(points, (points/total_cars))
+print('Classified cars:')
+for car_class in classified_cars:
+  print(f'{car_class}: {classified_cars[car_class]}')
+  print(f'Corectly classified: {correct_classified_cars[car_class]}')
+  print(f'Total from class: {total_classified_cars[car_class]}')
+  print(f'Accuracy: {round(correct_classified_cars[car_class] / total_classified_cars[car_class] * 100, 2)}%')
+  print('----------------------------------------------')
+print(f'Total Accuracy: {round(correct / total_cars * 100, 2)}%')
