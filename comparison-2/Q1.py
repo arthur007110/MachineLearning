8819d78f55010ac22b6a23a1ecb1a7f02a28f06c
Ivan do Nascimento <nascimentodoivan@gmail.com>
2023-08-05 22:35:14 -0300

    Questão 5 e correção questão 1

--- comparison-2/Q1.py
+++ comparison-2/Q1.py
@@ -6,7 +6,44 @@ from scipy.stats import t, tstd
 import math, random
 
 
+def split(data,y, splits):
+    size = len(data)
+    list = [i for i in range(size)]
+    list.sort(key=lambda x: random.random())
+    split_size = math.floor(size/splits)
+    X = []
+    for i in range(splits):
+        X.append(list[i*split_size:(i+1)*split_size])
+    
+    size = len(y)
+    list = [i for i in range(size)]
+    list.sort(key=lambda x: random.random())
+    split_size = math.floor(size/splits)
+    y = []
+    for i in range(splits):
+        y.append(list[i*split_size:(i+1)*split_size])
+    return X, y
 
+
+# function to partition the data into train and test sets based on the train_size in percentage
+def partition(X, y, train_size, ):
+  x_train = []
+  x_test = []
+  y_train = []
+  y_test = []
+  size = len(X)
+  list = [i for i in range(size)]
+  list.sort(key=lambda x: random.random())
+
+  train_size = math.floor(train_size * size)
+  for i in list:
+    if len(x_train) < train_size:
+      x_train.append(X[i])
+      y_train.append(y[i])
+    else:
+      x_test.append(X[i])
+      y_test.append(y[i])
+  return x_train, x_test, y_train, y_test
 
 def validatePredictions(predictions, correctValues): 
   correct = 0
@@ -36,34 +73,38 @@ for i in data:
 
 
 skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=1)
+
 neigh = KNeighborsClassifier(n_neighbors=1, weights="uniform")
 
 accuracy = []
 holdout_predictions = []
 skf_predictions = []
 f1 = []
-x_train,x_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=random.randint(1,10))
+
+
+x_train,x_test, y_train, y_test = partition(X, y, train_size=0.1)
 for i in range(10):
-    print(x_train[:5], y_train[:5])
-    print(len(x_train), len(x_test), len(y_train), len(y_test))
+    #print(x_train[:5], y_train[:5])
+    #print(len(x_train), len(x_test), len(y_train), len(y_test))
     neigh.fit(x_train, y_train)
     predictions = neigh.predict(x_test)
     holdout_predictions.append(validatePredictions(predictions, y_test))
 
 clf = svm.SVC(kernel='linear', C=10).fit(x_train, y_train)
-print("Media 10-CV: ", clf.score(x_test, y_test))
+#print("Media 10-CV: ", clf.score(x_test, y_test))
 
-print(holdout_predictions)
+#print(holdout_predictions)
 media = sum(holdout_predictions)/len(holdout_predictions)
-print("Media holdout: ", media)
+#print("Media holdout: ", media)
+train_index, test_index = split(X, y, 10)
 
-for train_index, test_index in skf.split(X,y):
-    print( len(train_index),  len(test_index)/(len(train_index)+len(test_index))*100)
+for j in range(len(train_index)):
+    #print( len(train_index),  len(test_index)/(len(train_index)+len(test_index))*100)
     x_train_fold, x_test_fold, y_train_fold, y_test_fold = [], [], [], []
-    for i in train_index:
+    for i in train_index[j]:
       x_train_fold.append(X[i])
       y_train_fold.append(y[i])
-    for i in test_index:
+    for i in test_index[j]:
       x_test_fold.append(X[i])
       y_test_fold.append(y[i])
 
---
+++ comparison-2/Q5.py
@@ -0,0 +1,66 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import StratifiedKFold, train_test_split
+from sklearn.metrics import f1_score, mean_squared_error
+from sklearn import svm
+from scipy.stats import t, tstd
+import math, random
+import pandas as pd
+import numpy as np
+
+
+def validatePredictions(predictions, correctValues): 
+  correct = 0
+  incorrect = 0
+  for i in range(len(predictions)):
+    if predictions[i] != correctValues[i]:
+      incorrect += 1
+    else:
+      correct += 1
+  #print("Correct: ", correct, "Incorrect: ", incorrect, "Accuracy: ", correct/(correct+incorrect))
+  return correct/(correct+incorrect)
+
+data_url = "http://lib.stat.cmu.edu/datasets/boston"
+raw_df = pd.read_csv(data_url, sep="\s+", skiprows=22, header=None)
+print(raw_df[:1])
+data = np.hstack([raw_df.values[::2, :], raw_df.values[1::2, :2]])
+target = raw_df.values[1::2, 2]
+
+parameter_amount = 13
+index_of_class = 0
+y = []  #y representa as classes
+X = [] #X representa as caracteristicas
+
+for i in data:
+  X.append([float(j) for j in i])
+
+intervals = list(range(int(min(target)),int(max(target)), 10))
+#print(intervals)
+
+for i in target:
+  if (i >= intervals[0] and i < intervals[1]):
+    y.append(0)
+  elif (i >= intervals[1] and i < intervals[2]):
+    y.append(1)
+  elif (i >= intervals[2] and i < intervals[3]):
+    y.append(2)
+  elif (i >= intervals[3] and i < intervals[4]):
+    y.append(3)
+  else:
+    y.append(4)
+
+#rmse = []
+rmse_lib = [] #RMSE com a biblioteca
+x_train,x_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=10)
+for j in range(100):
+    for i in range(1,6):
+       # print(x_train[:5], y_train[:5])
+        #print(len(x_train), len(x_test), len(y_train), len(y_test))
+        neigh = KNeighborsClassifier(n_neighbors=i, weights="uniform")
+        neigh.fit(x_train, y_train)
+        predictions = neigh.predict(x_test)
+        #print("predictions: ", predictions)
+        #rmse.append(np.sqrt(np.mean((y_test - predictions) ** 2)))
+        rmse_lib.append(mean_squared_error(y_test, predictions)) #RMSE com a biblioteca
+
+#print("rsme: ", rmse)
+print("rsme_lib: ", rmse_lib)
